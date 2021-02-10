1012e148eaac84eb3caa2405dce931033cc50290
Beren Boden <beren.boden@gmail.com>
2021-02-10 15:14:53 +1300

    added sidebar component and styling | cleaned out react-app template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Sidebar from './components/sidebar/Sidebar'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Discord clone</h1>
+    <div className="app">
+      <Sidebar />
     </div>
   );
 }
